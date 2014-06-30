8af0d9e14602f9d7aa241d7e23a84af76bd224be
joinabhimanyu <joinabhimanyu@gmail.com>
2014-06-30 17:57:06 +0530

    views added

--- Gulpfile.js
+++ Gulpfile.js
@@ -9,6 +9,26 @@ function startExpress() {
 	var express = require('express');
 	var app = express();
 	app.use(express.static(__dirname));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+
+	var router = express.Router();
+
+	router.use(function(req, res, next) {
+		console.log(req.method, req.url);
+		next();
+	});
+
+	router.get('/home', function(req, res) {
+		res.render('home');
+	});
+
+	router.get('/about', function(req, res) {
+		res.render('about');
+	});
+
+	app.use('/app', router);
+
 	app.listen(4000);
 }
 
